456b6f74c5beff512d2cba8aabe572e90cbd1f67
Danny Soto <87dsoto@gmail.com>
2020-01-22 18:09:50 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,6 @@ require 'pry'
 katz_deli = []
 
 def line(katz_deli)
-  binding.pry
   if katz_deli.length == 0
     puts "The line is currently empty."
   else 
@@ -17,13 +16,13 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  return "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
   if katz_deli.length == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else 
-    return "Now serving #{katz_deli[0]}."
+    puts "Now serving #{katz_deli[0]}."
   end
 end
