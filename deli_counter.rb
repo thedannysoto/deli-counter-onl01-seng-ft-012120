a68e16faa4a999500b84b4d510a51167071c27e9
Danny Soto <87dsoto@gmail.com>
2020-01-22 18:12:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.size == 0
     puts "The line is currently empty."
   else 
     puts "Now serving #{katz_deli[0]}."
