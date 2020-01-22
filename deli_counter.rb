ab00fa554aa3089044c511982f4c31e9a39b28d1
Danny Soto <87dsoto@gmail.com>
2020-01-22 17:25:18 +0000

    Automatically backed up by Learn

---
+++ deli_counter.rb
@@ -0,0 +1,14 @@
+katz_deli = []
+
+def line(katz_deli)
+  
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  return "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+
+def now_serving(katz_deli)
+  
+end
