e6eedb608f838b266d6c585a36471126eeabb676
Danny Soto <87dsoto@gmail.com>
2020-01-22 17:34:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,14 @@
 katz_deli = []
 
 def line(katz_deli)
-  
+  if katz_deli.length == 0
+    return "The line is currently empty."
+  else 
+    string = "The line is currently: "
+    katz_deli.each_with_index do |name, i|
+      string += "#{i+1}. #{name}"
+  end
+  return string
 end
 
 def take_a_number(katz_deli, name)
