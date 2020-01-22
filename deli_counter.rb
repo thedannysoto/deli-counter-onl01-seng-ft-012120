79f05829ea768ce050fe4c75a1201cf83d0a890b
Danny Soto <87dsoto@gmail.com>
2020-01-22 17:36:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,14 +2,14 @@ katz_deli = []
 
 def line(katz_deli)
   if katz_deli.length == 0
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else 
     string = "The line is currently: "
     katz_deli.each_with_index do |name, i|
       string += "#{i+1}. #{name}"
     end
   end
-  return string
+  puts string
 end
 
 def take_a_number(katz_deli, name)
